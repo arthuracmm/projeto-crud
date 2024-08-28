ef956c971439d94f3c9256b88246299ff3d267bc
Arthur Cesar <130599250+arthuracmm@users.noreply.github.com>
2024-08-27 23:21:59 -0300

    adicionando filtro e paginação dos produtos

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -4,42 +4,116 @@ import { Link } from "react-router-dom";
 import './home.css'
 import axios from "axios";
 
+const ITEMS_PER_PAGE = 5;
+
 function Home(){
 
     const[posts, setPosts] = useState([])
+    const [currentPage, setCurrentPage] = useState(1);
+    const [originalPosts, setOriginalPosts] = useState([]);
+    const [sortPrice, setSortPrice] = useState(false);
+    const [sortName, setSortName] = useState(false);
 
     useEffect(() =>{
-        // puxando Api para o 
+        // puxando Api para o projeto
         axios.get('https://api.escuelajs.co/api/v1/products')
         .then((response) =>{
             setPosts(response.data)
+            setOriginalPosts(response.data)
         })
         .catch(() =>{
             console.log('deu errado')
         })
     }, [])
 
+    function deletePost(id){
+        axios.delete(`https://api.escuelajs.co/api/v1/products/${id}`)
+
+        setPosts(posts.filter(post => post.id !== id))
+    }
+
+    const truncateText = (text, limit) => {
+        if (text.length > limit) {
+            return text.substring(0, limit) + '...';
+        }
+        return text;
+    };
+
+    const indexOfLastItem = currentPage * ITEMS_PER_PAGE;
+    const indexOfFirstItem = indexOfLastItem - ITEMS_PER_PAGE;
+    const currentPosts = posts.slice(indexOfFirstItem, indexOfLastItem);
+
+    const totalPages = Math.ceil(posts.length / ITEMS_PER_PAGE);
+
+    const changePage = (pageNumber) => {
+        setCurrentPage(pageNumber);
+      };
+
+    const handleSortChange = () => {
+        let sortedPosts = [...originalPosts];
+    
+        if (sortPrice) {
+          sortedPosts.sort((a, b) => a.price - b.price);
+        }
+    
+        if (sortName) {
+          sortedPosts.sort((a, b) => a.title.localeCompare(b.title));
+        }
+    
+    setPosts(sortedPosts);
+    
+    };
+
+    useEffect(() => {
+        handleSortChange();
+    }, [sortPrice, sortName]);
+
+
     return(
         <div>
             <HeaderMain />
+            
             <main>
+                
+                <div className="buttons-filters">
+                <label>
+                    <input
+                    type="checkbox"
+                    checked={sortPrice}
+                    onChange={() => setSortPrice(!sortPrice)}
+                    />
+                    Filtrar Por Menor Valor
+                </label>
+                <label>
+                    <input
+                    type="checkbox"
+                    checked={sortName}
+                    onChange={() => setSortName(!sortName)}
+                    />
+                    Filtar Por Ordem
+                </label>
+                </div>
+                
                 <div className="cards">
                     
-                    {posts.map((post, key)=> {
+                    {currentPosts.map((post) => {
                         return (
                             <div className="card">
 
                                 <div className="left-card">
+                                    <Link to = {{pathname:`/produtos/${post.id}`}}>
                                     <img src={post.images[0]}/>
+                                    </Link>
                                 </div>
 
                                 <div className="right-card">
                                     <header className="homeHeader">
-                                        <h2>{post.title}</h2>
+                                        <h2>{truncateText(post.title, 30)}</h2>
+                                        <p>R${post.price},00</p>
                                     </header>
                                     <div className="line"></div>
 
-                                    <p>{post.description}</p>
+                                    <p>{truncateText(post.description, 90)}</p>
 
                                     <div className="btns">
 
@@ -56,9 +130,7 @@ function Home(){
                                         </div>
 
                                         <div className="btnDelete">
-                                            <Link to = '/delete'>
-                                            <button className="btn">Deletar</button>
-                                            </Link>
+                                            <button className="btn" onClick={() => {deletePost(post.id)}}>Deletar</button>
                                         </div>
                                 
                                     </div>
@@ -66,11 +138,22 @@ function Home(){
                             </div>
                         )
                     })}
+
                     
-                    
-                    
+
                 </div>
             </main>
+            <div className="pagination">
+                    {Array.from({ length: totalPages }, (_, index) => (
+                        <button
+                        key={index}
+                        className={`page-button ${currentPage === index + 1 ? 'active' : ''}`}
+                        onClick={() => changePage(index + 1)}
+                        >
+                        {index + 1}
+                        </button>
+                    ))}
+            </div>
         </div>
     )
 }
